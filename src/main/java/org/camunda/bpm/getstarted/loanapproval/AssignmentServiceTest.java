d865623c43855e604b3e097c18968103fe6a0351
shaoliang <shaoliang.jia@gmail.com>
2022-07-08 22:51:49 +0800

    update

---
+++ src/main/java/org/camunda/bpm/getstarted/loanapproval/AssignmentServiceTest.java
@@ -0,0 +1,11 @@
+package org.camunda.bpm.getstarted.loanapproval;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssignmentServiceTest {
+     @Test
+     void getCandidateGroups() {
+     }
+}
